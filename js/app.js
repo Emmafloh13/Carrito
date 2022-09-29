5c783238069f88d6655bff84a54e1f14fe9398ee
Emmafloh13 <emmafh342@gmail.com>
2022-09-29 13:12:46 -0500

    Leer los datos del curso seleccionado

--- js/app.js
+++ js/app.js
@@ -7,13 +7,31 @@ cargarEventListener();
 function cargarEventListener(){
     listaCursos.addEventListener('click', agregarCurso); 
 }
-function agregarCurso(e){
-    console.log(e.target.classList);
 
-}
+
+//Funciones
 function agregarCurso(e){
     e.preventDefault();
+
     if(e.target.classList.contains('agregar-carrito')){
-    console.log('Presionaste el boton');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatoscurso(cursoSeleccionado);
+
     }
 }
+    //leer el contenido del HTML al que le dimos click y ectrae la informacon del curso
+function leerDatoscurso(curso){
+        console.log(curso);
+
+      //crear un objeto con el cotenido del curso actual
+        const infoCurso = {
+            imagen: curso.querySelector('img').src,
+            titulo: curso.querySelector('h4').textContent,
+            precio: curso.querySelector('.precio span').textContent,
+            id: curso.querySelector('a').getAttribute('data-id'),
+     
+        }
+        console.log(infoCurso)
+    }
+
+
